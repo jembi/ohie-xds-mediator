ce382c48878a34fb4855a8014332031e110dbd51
Ryan Crichton <rg.crichton@gmail.com>
2014-10-16 15:21:59 +0200

    Added JAXB marshaling and unmarshaling of XDS.b objects with a basic orchestration.

--- src/main/java/org/openhim/mediator/xds/ValidateAndEnrichClient.java
+++ src/main/java/org/openhim/mediator/xds/ValidateAndEnrichClient.java
@@ -1,16 +1,17 @@
 package org.openhim.mediator.xds;
 
-import java.util.HashMap;
-import java.util.Map;
+import ihe.iti.xds_b._2007.ObjectFactory;
+import ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType;
 
-import org.mule.api.MuleException;
+import org.apache.log4j.Logger;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
-import org.mule.module.client.MuleClient;
 import org.openhim.mediator.mule.MediatorMuleTransformer;
 
 
 public class ValidateAndEnrichClient extends MediatorMuleTransformer {
+	
+	Logger log = Logger.getLogger(this.getClass());
 	
 	@Override
 	public Object transformMessage(MuleMessage message, String outputEncoding)
@@ -36,6 +37,14 @@ public class ValidateAndEnrichClient extends MediatorMuleTransformer {
 		} catch (MuleException e) {
 			throw new TransformerException(this, e.getCause());
 		}*/
+		
+		ProvideAndRegisterDocumentSetRequestType pnr = (ProvideAndRegisterDocumentSetRequestType) message.getPayload();
+		
+		int size = pnr.getDocument().size();
+		log.info("Number of documents in this request: " + size);
+		
+		ObjectFactory of = new ObjectFactory();
+		message.setPayload(of.createProvideAndRegisterDocumentSetRequest(pnr));
 		return message;
 	}
 
